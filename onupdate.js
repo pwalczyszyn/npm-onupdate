45ef481e23d0abe27d9ade76ed5fdc6b798daf7f
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-10-18 16:07:26 +0200

    list wip

--- onupdate.js
+++ onupdate.js
@@ -6,5 +6,6 @@ var program = require('commander');
 
 program
     .version('0.0.1')
-    .option('add <package>', 'Register new alert for specified package <package>', require('./lib/commands/add'))
+    .option('-l, --list <package>', 'List all packages registered with your accont.', require('./lib/commands/list'))
+    .option('-a, --add <package>', 'Register new alert for specified package <package>.', require('./lib/commands/add'))
     .parse(process.argv);
