c6d9ad30f46d51bef8396a6f62aeea49c4df2aa0
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-10-18 22:47:18 +0200

    remote url, alerts list formatting

--- onupdate.js
+++ onupdate.js
@@ -4,8 +4,19 @@
 
 var program = require('commander');
 
+program.version('0.0.1');
+
+//    .option('-l, --list', 'List all packages registered with your accont.', require('./lib/commands/list'))
+//    .option('-a, --add <package>', 'Register new alert for specified package <package>.', require('./lib/commands/add'))
+
 program
-    .version('0.0.1')
-    .option('-l, --list <package>', 'List all packages registered with your accont.', require('./lib/commands/list'))
-    .option('-a, --add <package>', 'Register new alert for specified package <package>.', require('./lib/commands/add'))
-    .parse(process.argv);
+    .command('add <package>')
+    .description('Register new alert for specified <package>.')
+    .action(require('./lib/commands/add'));
+
+program
+    .command('list')
+    .description('List all packages registered with your accont.')
+    .action(require('./lib/commands/list'));
+
+program.parse(process.argv);
